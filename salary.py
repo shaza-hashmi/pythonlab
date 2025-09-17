5756cbaf7f27c0d69b1db6e2b421cbb6c1ac536c
Your Name <you@example.com>
2025-09-17 11:56:19 +0530

    hrehE

---
+++ salary.py
@@ -0,0 +1,11 @@
+basic = float(input("Enter basic pay: "))
+hra = 0.1 * basic
+ta = 0.05 * basic
+salary = basic + hra + ta
+print("Total Salary:", salary)
+
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                               
